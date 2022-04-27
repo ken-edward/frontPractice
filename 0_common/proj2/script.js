40bf53f7bc6ff454efd56d5f2ad90d8b23ee1ec9
Ken <92659102+ken-edward@users.noreply.github.com>
2022-04-27 15:09:09 +0000

    ox quiz

--- 0_common/proj2/script.js
+++ 0_common/proj2/script.js
@@ -1,10 +1,36 @@
+
+
+// wasd
 var up = false,
     right = false,
     down = false,
     left = false,
-    x = window.innerWidth/2, // - 130/2,
-    y = window.innerHeight/2 // - 130/2
-document.addEventListener('keydown',press)
+    x = window.innerWidth/2,
+    y = window.innerHeight/2,
+		ans = true;
+document.addEventListener('keydown',press);
+document.getElementById("btn1").addEventListener('click',hello);
+
+function checkAnswer() {
+	ans = x <= window.innerWidth/2 ? true : false;
+
+	var div = document.querySelector('#selection')
+	
+	if (ans) {
+		div.innerText="현재 선택: O";
+	} else {
+		div.innerText="현재 선택: X";
+	}
+}
+
+function hello() {
+  if (ans) {
+		location.href = "result.html?1"
+	} else {
+		location.href = "result.html?0"
+	}
+}
+
 function press(e){
   if (e.keyCode === 38 /* up */ || e.keyCode === 87 /* w */ || e.keyCode === 90 /* z */){
     up = true
@@ -35,7 +61,7 @@ function release(e){
   }
 }
 function gameLoop(){
-  var div = document.querySelector('div')
+  var div = document.querySelector('.move')
   if (up){
     y = y - 10
   }
@@ -50,6 +76,7 @@ function gameLoop(){
   }
   div.style.left = x+'px'
   div.style.top = y+'px'
+	checkAnswer();
   window.requestAnimationFrame(gameLoop)
 }
 window.requestAnimationFrame(gameLoop)
